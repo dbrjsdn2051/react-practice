9dd2cb6cffe4c5b7b888563cc327eaccf86197da
dbrjsdn2051 <dbrjdn2051@naver.com>
2025-12-25 23:06:53 +0900

    init

---
+++ section05/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Header</h1>
+        </header>
+    )
+}
+
+function App() {
+    return (
+        <>
+            <Header />
+            <h1>Hello React</h1>
+        </>
+    )
+}
+
+export default App
