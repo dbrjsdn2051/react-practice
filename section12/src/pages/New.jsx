7a9bbce25fc17d4edfe225a20a3b521c3fe40007
dbrjsdn2051 <dbrjdn2051@naver.com>
2026-01-11 22:37:43 +0900

    feat: new page ui add

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,5 +1,12 @@
+import Header from "../component/Header.jsx";
+import Button from "../component/Button.jsx";
+import Editor from "../component/Editor.jsx";
+
 const New = () => {
-    return <div>New</div>
+    return <div>
+        <Header title={"새 일기 쓰기"} leftChild={<Button text={"< 뒤로가기"} /> }/>
+        <Editor />
+    </div>
 }
 
 export default New;
